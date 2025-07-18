8feb8fee0dbda25da695c2cc885dbd9e0d3e9f78
mercally1 <jmercally@gmail.com>
2025-07-17 21:45:46 -0600

    ejercicio fizz buzz

---
+++ proyectosc#/fizzbuzz/Program.cs
@@ -0,0 +1,29 @@
+﻿
+class Program
+{
+    public static void Main(string[] args)
+    {
+        for (int i = 1; i <= 100; i++)
+        {
+            var divisortres = i % 3 == 0;
+            var divisorcinco = i % 5 == 0;
+
+            if (divisortres && divisorcinco)
+            {
+                Console.WriteLine($"{i}.- Fizzbuzz\n");
+            }
+            else if (divisortres)
+            {
+                Console.WriteLine($"{i}.- Fizz\n");
+            }
+            else if (divisorcinco)
+            {
+                Console.WriteLine($"{i}.- buzz \n");
+            }
+            else
+            {
+                Console.WriteLine($"{i}\n");
+            }
+        }
+    }
+}
